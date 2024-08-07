e54d9afcf7b51d55b5ef85efaf12f4c7518b2183
Shivam Prajapati <128581121+ShivamDeadlocker@users.noreply.github.com>
2024-08-07 09:47:24 +0530

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ class MytestCase(unittest.TestCase):
     def test_to_upper(self):
         name="siv"
         upper_name=to_upper(name)
-        self.assertEqual(upper_name,'siv')
+        self.assertEqual(upper_name,'SIV')
         
 if __name__ == "__main__":
     unittest.main()
